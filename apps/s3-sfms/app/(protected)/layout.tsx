336a3771efa1e8e304b190f96dad2973f19d705b
Luis Dourado <lfaddourado@gmail.com>
2024-02-03 19:28:04 -0300

    refactor(auth): check on the db instead of the jwt the user session on protected layout

--- apps/s3-sfms/app/(protected)/layout.tsx
+++ apps/s3-sfms/app/(protected)/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const user = await readSession();
+	const { user } = await validateRequest();
 	if (!user) {
 		redirect('/auth/login');
 	}
