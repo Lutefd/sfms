76d2e525cbe2c109b171c7f542936e14b9fda979
Luis Dourado <lfaddourado@gmail.com>
2024-02-03 17:21:57 -0300

    chore(db): update database driver to turso's sqlite and credentials

--- apps/s3-sfms/drizzle.config.ts
+++ apps/s3-sfms/drizzle.config.ts
@@ -4,15 +4,12 @@ import { env } from '@/env';
 
 export default {
 	schema: './server/db/schema.ts',
-	driver: 'mysql2',
+	driver: 'turso',
 	dbCredentials: {
 		// connectionString: env.DATABASE_URL,
 
-		host: env.DB_HOST,
-		port: 3306,
-		user: env.DB_USER,
-		password: env.DB_PASSWORD,
-		database: env.DB_DATABASE,
+		url: env.DB_URL,
+
 		//#ts-ignore - ssl is not in the types
 	},
 	tablesFilter: ['dz_*'],
--- apps/s3-sfms/server/db/index.ts
+++ apps/s3-sfms/server/db/index.ts
@@ -1,57 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
-import { drizzle as drizzleDev, type MySql2Database } from 'drizzle-orm/mysql2';
-// import {
-// 	drizzle as drizzle,
-// 	type PlanetScaleDatabase,
-// } from 'drizzle-orm/planetscale-serverless';
-import mysql from 'mysql2/promise';
-
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+import * as schema from './schema';
 import { env } from '@/env';
-
-import * as myschema from '@/server/db/schema';
-
-export interface Global {
-	cachedDbPromise: MySql2Database<typeof myschema> | null;
-}
-
-async function connectToDatabase() {
-	if ((globalThis as any).cachedDbPromise) {
-		console.log('using cached db');
-		return (globalThis as any).cachedDbPromise as MySql2Database<
-			typeof myschema
-		>;
-	}
-	console.log('connecting to db');
-	const client = await mysql.createConnection({
-		host: env.DB_HOST,
-		port: 3306,
-		user: env.DB_USER,
-		password: env.DB_PASSWORD,
-		database: env.DB_DATABASE,
-	});
-	(globalThis as any).cachedDbPromise = drizzleDev(client, {
-		schema: myschema,
-		mode: 'default',
-	});
-	return (globalThis as any).cachedDbPromise as MySql2Database<
-		typeof myschema
-	>;
-}
-
-const authQueryClient = mysql.createPool({
-	host: env.DB_HOST,
-	port: 3306,
-	user: env.DB_USER,
-	password: env.DB_PASSWORD,
-	database: env.DB_DATABASE,
+const client = createClient({
+	url: env.DB_URL,
+	authToken: env.DB_AUTH_TOKEN,
 });
-
-export const authDb = drizzleDev(authQueryClient, {
-	schema: myschema,
-	mode: 'default',
-	logger: true,
+export const db = drizzle(client, {
+	schema,
 });
-
-export const dbPromise = connectToDatabase();
