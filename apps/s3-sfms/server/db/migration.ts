10cfeb35b3b8879b2e0117e65f15535cb3d1648e
Luis Dourado <lfaddourado@gmail.com>
2024-02-05 15:04:10 -0300

    refactor(migration): update migration driver

--- apps/s3-sfms/server/db/migration.ts
+++ apps/s3-sfms/server/db/migration.ts
@@ -1,22 +1,33 @@
-import { env } from '@/env';
-import { MySql2Database, drizzle } from 'drizzle-orm/mysql2';
-import { migrate } from 'drizzle-orm/mysql2/migrator';
+import { migrate } from 'drizzle-orm/libsql/migrator';
 import 'dotenv/config';
-import mysql from 'mysql2/promise';
+import { LibSQLDatabase, drizzle } from 'drizzle-orm/libsql';
+import { Client, createClient } from '@libsql/client';
+import * as schema from './schema';
+import { env } from '@/env';
+const client = createClient({
+	url: env.DB_URL,
+	authToken: env.DB_AUTH_TOKEN,
+});
+export const db = drizzle(client, {
+	schema,
+});
+
 async function main() {
-	const sql = await mysql.createConnection({
-		uri: process.env.DB_URL!,
+	const sql = createClient({
+		url: env.DB_URL,
+		authToken: env.DB_AUTH_TOKEN,
 	});
+
 	const db = drizzle(sql);
 	await migrateDb(db, sql);
 }
 
 async function migrateDb(
-	db: MySql2Database<Record<string, unknown>>,
-	sql: mysql.Connection
+	db: LibSQLDatabase<Record<string, unknown>>,
+	sql: Client
 ) {
 	await migrate(db, { migrationsFolder: 'drizzle' });
-	await sql.end();
+	await sql.close();
 }
 
 main();
