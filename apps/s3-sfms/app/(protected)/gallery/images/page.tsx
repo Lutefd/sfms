178d9d857de5f3632e0394ee1b7635c4108daa9c
Luis Dourado <lfaddourado@gmail.com>
2024-03-02 11:59:29 -0300

    feat(image view): add image view component and query invalidation
    This commit adds the image view component to its page and pass in the tag invalidation to the upload button

--- apps/s3-sfms/app/(protected)/gallery/images/page.tsx
+++ apps/s3-sfms/app/(protected)/gallery/images/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import UploadButton from '../_components/upload-button';
 import { Input } from '@/components/ui/input';
+import { getAllImagesFromUserId } from '@/lib/files';
+import { readSession } from '@/actions/session';
+import ImagesView from './_components/images-view';
 
-function ImagesPage() {
+async function ImagesPage() {
+	const user = await readSession();
 	return (
 		<div className="flex flex-col px-4">
 			<div className="py-4 flex w-full justify-between items-center">
 				<h1 className="text-2xl font-bold">Imagens</h1>
-				<UploadButton />
+				<UploadButton fileType="image" />
 			</div>
+			<ImagesView id={user!.id!} />
 		</div>
 	);
 }
